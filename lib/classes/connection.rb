70d3444ecfaf9ab003bba542d13097aebc201458
Gerald Huff <ghuff@teslamotors.com>
2013-06-21 14:37:54 -0700

    Do not reuse cached connection for longer than authenticated

--- lib/classes/connection.rb
+++ lib/classes/connection.rb
@@ -5,6 +5,7 @@ module Quickbase
     end
     attr_reader :username, :password, :hours, :apptoken, :dbid, :org, :fields
     @http = nil
+    @http_acquired = nil
 
     def self.expectant_reader(*attributes)
       attributes.each do |attribute|
@@ -15,11 +16,13 @@ module Quickbase
       end
     end
     expectant_reader :username, :password, :hours, :apptoken, :dbid, :org
+    @hours = 12 # qb default
 
     def initialize(options = {})
       [:username, :password, :hours, :apptoken, :dbid, :org].each do |attr|
         instance_variable_set "@#{attr}", (options[attr].nil? ? Quickbase::Connection.send(attr) : options[attr])
       end
+      @hours = options[:hours] if options.has_key? :hours
       instance_variable_set "@org", "www" if org.nil?
       instance_variable_set "@fields", {}
       Quickbase::API.new(self).get_schema if options.has_key? :load_schema
@@ -37,7 +40,8 @@ module Quickbase
     end
 
     def http
-      return @http if @http
+      return @http if @http and @http_acquired > @hours.hours.ago
+      @http_acquired = Time.now
       @http = Quickbase::HTTP.new(instantiate)
     end
 
